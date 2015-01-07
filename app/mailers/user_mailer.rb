e61d70d855b9ea3bd54c702bce94dc039aa8193f
vichugunov <chu-vik@mail.ru>
2015-01-07 06:13:44 +0300

    Test mailer added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
 class UserMailer < ActionMailer::Base
+  @@pdf_name = 'Tokarev.pdf'
+  @@pdf_path = Rails.root.to_s + "/" + @@pdf_name
   default from: "\"DCC RAS\" <lcmi@ccas.ru>"
 
-  def imta_first_call(user_mail)
-  	attachments['imta2015-cfp-1.pdf'] = File.read('imta2015-cfp-1.pdf');
-    mail(to: user_mail, subject: 'IMTA-5 2015: 1st Call for Participation')
+  def imta_second_call(user_mail)
+  	pdf = File.read(@@pdf_path, mode: "rb") 
+  	#pdf = render_to_string :pdf => @@pdf_name
+    attachments[@@pdf_name] = pdf if pdf.present?
+    mail(to: user_mail, subject: 'IMTA-5 2015: 2nd Call for Participation')
   end
 end
